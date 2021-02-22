02510a2874af7222bd0189ebd89aa5275e434ea6
rusolm <leandromarquesp88@gmail.com>
2021-02-23 00:51:01 +0100

    feat: modify store, just one reducer needed

--- src/common/store/index.js
+++ src/common/store/index.js
@@ -2,7 +2,5 @@ import { configureStore } from '@reduxjs/toolkit';
 import todoReducer from './todoSlice';
 
 export default configureStore({
-  reducer: {
-    todo: todoReducer
-  }
+  reducer: todoReducer
 });
