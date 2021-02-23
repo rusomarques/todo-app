608b91bb25799c0eeec31d603d44ad55f2b70b4e
rusolm <leandromarquesp88@gmail.com>
2021-02-23 08:42:38 +0100

    refactor(redux): todos reducer

---
+++ src/common/store/reducer.js
@@ -0,0 +1,33 @@
+import { getFromLocalStorage } from '../utils/getFromLocalStorage';
+import { ACTION_TYPES } from './actionTypes';
+
+const initialState = getFromLocalStorage([]);
+
+const editTodo = (state, action) =>
+  state.reduce(
+    (todos, todo) =>
+      todo.id === action.payload.id ? [...todos, { ...todo, task: action.payload.task }] : [...todos, todo],
+    []
+  );
+
+const toggleIsDone = (state, action) =>
+  state.map(todo => (todo.id === action.payload.id ? { ...todo, done: !todo.done } : todo));
+
+/** ************* REDUCER ************************* */
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ACTION_TYPES.ADD_TODO:
+      return [...state, action.payload];
+    case ACTION_TYPES.REMOVE_TODO:
+      return state.filter(todo => todo.id !== action.payload);
+    case ACTION_TYPES.EDIT_TODO:
+      return editTodo(state, action);
+    case ACTION_TYPES.TOGGLE_IS_DONE:
+      return toggleIsDone(state, action);
+
+    default:
+      return state;
+  }
+};
+
+export default reducer;
