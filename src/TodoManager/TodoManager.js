3ed539fe9786d028a72af9f5b4abfedf42df24f2
rusolm <leandromarquesp88@gmail.com>
2021-02-23 01:12:39 +0100

    feat(TodoResume): show header with number of todos

--- src/TodoManager/TodoManager.js
+++ src/TodoManager/TodoManager.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Form } from './Form/Form';
 import { TodoList } from './TodoList/TodoList';
+import { TodoResume } from './TodoResume/TodoResume';
 
 export const TodoManager = () => (
   <section className="todo-manager">
-    <header>Todo APP</header>
+    <TodoResume />
     <TodoList />
     <Form />
   </section>
---
+++ src/TodoManager/TodoResume/TodoResume.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+export function TodoResume() {
+  const todos = useSelector(state => state.todos);
+  const numberOfTodos = todos.length;
+  const isPendingTodos = numberOfTodos > 0;
+  const headerMessage = isPendingTodos
+    ? `You have ${numberOfTodos} Todo${numberOfTodos > 1 ? 's' : ''}`
+    : 'Add new Todo';
+
+  return (
+    <header>
+      <h2>{headerMessage}</h2>
+    </header>
+  );
+}
