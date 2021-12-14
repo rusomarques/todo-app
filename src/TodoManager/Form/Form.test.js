b9dfddca0dacd01c1a902f32546d3c2507333512
Leandro Marques <leandromarquesp88@gmail.com>
2021-12-14 02:01:55 +0100

    add tests to form

--- src/TodoManager/Form/Form.test.js
+++ src/TodoManager/Form/Form.test.js
@@ -4,6 +4,13 @@ import { render, screen } from '../../test/test-utils';
 import { getRandomWordsBetween } from '../../test/helpers';
 import { Form } from './Form';
 
+test('input is initialized empty', () => {
+  render(<Form />);
+  const input = screen.getByRole('textbox', { name: /new todo/i });
+
+  expect(input).toHaveValue('');
+});
+
 test('input is cleared after submitting', () => {
   render(<Form />);
   const newTodo = getRandomWordsBetween(1, 5);
@@ -12,5 +19,36 @@ test('input is cleared after submitting', () => {
   userEvent.type(input, newTodo);
   userEvent.click(screen.getByRole('button', { name: /submit/i }));
 
-  expect(input).toBeEmpty();
+  expect(input).toHaveValue('');
+});
+
+test('input shows what user writes', () => {
+  render(<Form />);
+  const newTodo = getRandomWordsBetween(1, 5);
+
+  const input = screen.getByRole('textbox', { name: /new todo/i });
+  userEvent.type(input, newTodo);
+
+  expect(input).toHaveValue(newTodo);
+});
+
+test('submit button is disabled if there is no input introduced', () => {
+  render(<Form />);
+
+  const input = screen.getByRole('textbox', { name: /new todo/i });
+  const submitButton = screen.getByRole('button', { name: /submit/i });
+  expect(input).toHaveValue('');
+  expect(submitButton).toBeDisabled();
+});
+
+test('submit button is enabled when input is introduced', () => {
+  render(<Form />);
+
+  const newTodo = getRandomWordsBetween(1, 5);
+
+  const input = screen.getByRole('textbox', { name: /new todo/i });
+  userEvent.type(input, newTodo);
+  const submitButton = screen.getByRole('button', { name: /submit/i });
+
+  expect(submitButton).toBeEnabled();
 });
